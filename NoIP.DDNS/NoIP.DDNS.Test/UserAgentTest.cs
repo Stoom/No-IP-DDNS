6fc09c02fec8b78a4079479868f5fbe949b7809a
James Stumme <james@stumme.net>
2015-07-11 12:25:03 -0500

    Passed SetProgramUserAgentViaProgramNameAndReturnFullUserAgent

--- NoIP.DDNS/NoIP.DDNS.Test/UserAgentTest.cs
+++ NoIP.DDNS/NoIP.DDNS.Test/UserAgentTest.cs
@@ -21,5 +21,20 @@ namespace NoIP.DDNS.Test
                                                 currentVersion.Minor);
             Assert.AreEqual(expectedResults, ua.ToString());
         }
+
+        [TestMethod]
+        public void SetProgramUserAgentViaProgramNameAndReturnFullUserAgent()
+        {
+            const string programName = "UATest";
+            var ua = new UserAgent("llama");
+            ua.ProgramName = programName;
+
+            var currentVersion = GetType().Assembly.GetName().Version;
+            var expectedResults = String.Format("DDNS/1.0 (Windows NT 6.3; WOW64) {0}/{1}.{2}",
+                                                programName,
+                                                currentVersion.Major,
+                                                currentVersion.Minor);
+            Assert.AreEqual(expectedResults, ua.ToString());
+        }
     }
 }
