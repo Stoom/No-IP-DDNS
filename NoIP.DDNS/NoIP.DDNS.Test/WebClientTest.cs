af122794785c3886ca4705ac2c606f42a709d528
James Stumme <james@stumme.net>
2015-07-11 10:53:23 -0500

    Started ddns client userage test.

---
+++ NoIP.DDNS/NoIP.DDNS.Test/WebClientTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NoIP.DDNS.Test
+{
+    [TestClass]
+    public class WebClientTest
+    {
+        [TestMethod]
+        public void SetProgrameUserAgentAndReturnFullUserAgent()
+        {
+            var progameUserAgent = "DDnsTest";
+            var client = new Client(progameUserAgent);
+
+            var expectedResults = "DDNS/1.0 (Windows NT 6.3; WOW64) DDnaTest/1.0";
+            Assert.AreEqual(expectedResults, client.UserAgent);
+        }
+    }
+}
---
+++ NoIP.DDNS/NoIP.DDNS/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoIP.DDNS
+{
+    public class Client
+    {
+        protected readonly string AssemblyVersion;
+        private static readonly Assembly ddnsAssembly = Assembly.GetExecutingAssembly();
+
+        public string UserAgent
+        {
+            get { return _userAgent; }
+            set
+            {
+                _userAgent = string.Format("DDNS/{0} ({1}) {2}/{3}",
+                                            AssemblyVersion,
+                                            Environment.OSVersion)
+            }
+        }
+
+        private string _userAgent;
+
+        public Client(string userAgentName)
+        {
+            // Set assembly version
+            var version = ddnsAssembly.GetName().Version;
+            AssemblyVersion = String.Format("{0}.{1}", version.Major, version.Minor);
+        }
+    }
+}
