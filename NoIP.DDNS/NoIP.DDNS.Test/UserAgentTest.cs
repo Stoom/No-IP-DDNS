8a9bc917b5d4337ec4c6e1bc9b0247a6fe898e76
James Stumme <james@stumme.net>
2015-07-11 11:26:10 -0500

    Passed SetProgramUserAgentAndReturnFullUserAgent test.

---
+++ NoIP.DDNS/NoIP.DDNS.Test/UserAgentTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoIP.DDNS;
+
+namespace NoIP.DDNS.Test
+{
+    [TestClass]
+    public class UserAgentTest
+    {
+        [TestMethod]
+        public void SetProgramUserAgentAndReturnFullUserAgent()
+        {
+            const string programName = "UATest";
+            var ua = new UserAgent(programName);
+
+            var currentVersion = GetType().Assembly.GetName().Version;
+            var expectedResults = String.Format("DDNS/1.0 (Windows NT 6.2; WOW64) {0}/{1}.{2}",
+                                                programName,
+                                                currentVersion.Major,
+                                                currentVersion.Minor);
+            Assert.AreEqual(expectedResults, ua.ToString());
+        }
+    }
+}
