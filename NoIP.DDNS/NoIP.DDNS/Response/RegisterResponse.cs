ee916879ac9f51b0479fa609a6a1fd36929064cd
James Stumme <james@stumme.net>
2015-07-11 22:53:40 -0500

    Added missing dto.

---
+++ NoIP.DDNS/NoIP.DDNS/Response/RegisterResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace NoIP.DDNS.Response
+{
+    /// <remarks/>
+    [Serializable, XmlRoot("client")]
+    public partial class RegisterResponse
+    {
+        [XmlElement(ElementName = "id")]
+        public string Id { get; set; }
+        [XmlElement(ElementName = "key")]
+        public string Key { get; set; }
+    }
+}
