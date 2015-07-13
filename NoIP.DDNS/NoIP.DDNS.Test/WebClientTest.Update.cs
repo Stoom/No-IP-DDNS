9d81656cb0ab232f6e5ac46328c770e4e4472182
James Stumme <james@stumme.net>
2015-07-12 23:40:17 -0500

    Added UpdateSingleHostIpAddressSuccessfully test.

---
+++ NoIP.DDNS/NoIP.DDNS.Test/WebClientTest.Update.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoIP.DDNS.DTO;
+
+namespace NoIP.DDNS.Test
+{
+    public partial class WebClientTest
+    {
+        [TestMethod]
+        public void UpdateSingleHostIpAddressSuccessfully()
+        {
+            _client.Id = _noipClientId;
+            _client.Key = _noipClientKey;
+
+            Assert.IsTrue(_client.IsRegistered);
+
+            using (ShimsContext.Create())
+            {
+                ShimWebClient.AllInstances.DownloadStringString = (client, s) => "host1:1";
+
+                var host = new Host("Host1") {Address = IPAddress.Parse("127.0.0.1")};
+
+                AssertExtensions.DoesNotThrow<UpdateException>(_client.UpdateHost(host));
+            }
+        }
+    }
+}
