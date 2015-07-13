e228879f7567302874a00e2ad9c59ee511fb4ed8
James Stumme <james@stumme.net>
2015-07-13 06:48:55 -0500

    Finished writing UpdateMultipleHostsWithASingleIpAddressSuccessfully test.

--- NoIP.DDNS/NoIP.DDNS.Test/WebClientTest.Update.cs
+++ NoIP.DDNS/NoIP.DDNS.Test/WebClientTest.Update.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Net.Fakes;
 using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -24,6 +25,30 @@ namespace NoIP.DDNS.Test
                 var host = new Host("Host1") {Address = IPAddress.Parse("127.0.0.1")};
 
                 AssertExtensions.NoExpectedException<UpdateException>(() => _client.UpdateHost(host));
+            }
+        }
+
+        [TestMethod]
+        public void UpdateMultipleHostsWithASingleIpAddressSuccessfully()
+        {
+            _client.Id = _noipClientId;
+            _client.Key = _noipClientKey;
+
+            Assert.IsTrue(_client.IsRegistered);
+
+            using (ShimsContext.Create())
+            {
+                ShimWebClient.AllInstances.DownloadStringString = (client, s) => "host1:1";
+
+                var address = IPAddress.Parse("127.0.0.1");
+
+                var hosts = new HashSet<Host>
+                {
+                    new Host("Host1") { Address = address },
+                    new Host("Host2") { Address = address }
+                };
+
+                AssertExtensions.NoExpectedException<UpdateException>(() => _client.UpdateHost(hosts));
             }
         }
     }
